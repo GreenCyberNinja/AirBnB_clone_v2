29a027a3bfce0a98a411cbfdf26d385aa0490093
JwowSquared <Jdwagner1@hotmail.com>
2020-12-16 18:13:03 +0000

    changed scripts to normalize captialization, added semicolons and a few lines to the test setup as well, fixed typo in password for dev setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,8 @@
---script that preps a mysql server
-CREATE DATABASE IF NOT EXIST hbnb_test_db
-CREATE USER IF NOT EXIST 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd"
-GRANT ALL ON hbnb_test_db TO 'hbnb_test'@'localhost'
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost'
+--script that creates a test server for MySQL
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
