14e717a4f07ddb0cdeff342003a560ec7082f1cc
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-01-20 22:29:32 +0000

    put storagetype check in

--- models/state.py
+++ models/state.py
@@ -3,21 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+stortyp = getenv('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states", cascade="all, delete")
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="states", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """getter for FileStorage use"""
-        # potentially need to encase this in an if statement to protect
-        from models.base_model import storage
-        out = []
-        for obj in storage.all(City).values():
-            if obj.state_id == self.id:
-                out.append(obj)
-        return out
+    else:
+        @property
+        def cities(self):
+            """getter for FileStorage use"""
+            # potentially need to encase this in an if statement to protect
+            from models.base_model import storage
+            out = []
+            for obj in storage.all(City).values():
+                if obj.state_id == self.id:
+                    out.append(obj)
+            return out
