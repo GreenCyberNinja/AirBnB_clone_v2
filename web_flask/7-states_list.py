245fd3023f3bc476b78783971ae68284f0b67cc1
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-01-21 04:32:52 +0000

    pep*

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def statels():
-   db = storage.all(State).values()
-   return render_template('7-states_list.html', db=db)
+    db = storage.all(State).values()
+    return render_template('7-states_list.html', db=db)
+
 
 @app.teardown_appcontext
 def close_db(err):
